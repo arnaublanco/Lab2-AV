1eb70c86d9e760c4b59ddf1a23881d4447be67ff
Arnau <blancoarnau@gmail.com>
2021-10-13 18:37:48 +0200

    shaders

--- data/shaders/flat.fs
+++ data/shaders/flat.fs
@@ -1,7 +1,29 @@
 
+attribute vec3 a_vertex;
+attribute vec3 a_normal;
+attribute vec2 a_uv;
+attribute vec4 a_color;
+
+uniform vec3 u_camera_pos;
+
+uniform mat4 u_model;
+uniform mat4 u_viewprojection;
+
+//this will store the color for the pixel shader
+varying vec3 v_position;
+varying vec3 v_world_position;
+varying vec3 v_normal;
+varying vec2 v_uv;
+varying vec4 v_color;
+
 uniform vec4 u_color;
 
 void main()
 {
+	vec3 L = normalize(light_pos - v_world_position);
+	vec3 N = normalize(v_normal);
+	vec3 V = normalize(u_camera_position - v_world_position);
+	vec3 R = reflect(-L,N);
+	vec3 H = normalize(V+L);
 	gl_FragColor = u_color;
 }
